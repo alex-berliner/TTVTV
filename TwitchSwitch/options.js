ac830da68da3fc47bc2c4fc989ca8600442a8ea1
Alex <alexberliner@gmail.com>
2016-04-06 21:14:23 -0400

    Set up basic load/store

--- TwitchSwitch/options.js
+++ TwitchSwitch/options.js
@@ -1,14 +1,29 @@
-/**
- * Called when user requests to generate view preferences
- * based on history.
- */
-$("#gen-history").click(function () {
-	chrome.permissions.contains({
-		permissions : ['history']
-	}, history_callback);
-});
+// chrome MVC: https://developer.chrome.com/apps/app_frameworks
 
 var streamer_array;
+var streamer_html_list;
+
+init();
+
+function init(){
+    streamer_array = [];
+    streamer_html_list = $("#sortable");
+    restore_options();
+    
+    
+    // Called when user requests to generate 
+    // view preferences based on history.
+    $("#gen-history").click(function () {
+        if(streamer_array.length != 0){
+            return;
+        }
+        chrome.permissions.contains({
+            permissions : ['history']
+        }, history_callback);
+    });
+}
+
+
 /**
  * Called when chrome returns history.
  * Places all of the user's watched twitch streams into an array,
@@ -49,6 +64,30 @@ function history_callback(result) {
     }
 }
 
+
+/**
+ * Updates streamer list HTML
+ */
+function update_stream_list(){
+    streamer_html_list.empty();
+    for(var i = 0; i < streamer_array.length; i++){
+        streamer_html_list.append("<li class=\"ui-state-default\"><span class=\"ui-icon ui-icon-arrowthick-2-n-s\"></span>" + 
+        streamer_array[i].visited_count + " " + 
+        streamer_array[i].username + "</li>");
+    }
+}
+
+function sort_streamer_array(){
+    streamer_array.sort(function(a,b){
+        if (a.visited_count < b.visited_count) return 1;
+        if (a.visited_count > b.visited_count) return -1;
+        return 0;
+    });
+}
+
+/**
+ * 
+ */
 function get_valid_streams(potential_streamer_arr){
     console.log(potential_streamer_arr.length);
     chrome.runtime.sendMessage(
@@ -58,23 +97,9 @@ function get_valid_streams(potential_streamer_arr){
     },
     function(streamers){
         streamer_array = streamers;
-        // console.log(streamer_array[0]);
-        
-        //sort streamer array by view count
-        streamer_array.sort(function(a,b){
-            if (a.visited_count < b.visited_count) return 1;
-            if (a.visited_count > b.visited_count) return -1;
-            return 0;
-        });
-        
-        //add streamer list items to page
-        var streamer_html = $("#sortable");
-        for(var i = 0; i < streamer_array.length; i++){
-            // console.log(streamer_array[i].username)
-            streamer_html.append("<li class=\"ui-state-default\"><span class=\"ui-icon ui-icon-arrowthick-2-n-s\"></span>" + 
-            streamer_array[i].visited_count + " " + 
-            streamer_array[i].username + "</li>");
-        }
+        sort_streamer_array();
+        update_stream_list();
+        save_options();
     });
 }
 
@@ -88,46 +113,35 @@ var streamer_object = class {
 function url_username(username){
     return username.split("v/")[1];
 }
+//Saves options to chrome.storage.sync.
+function save_options() {
+    chrome.storage.sync.set({
+        "streamer_array" : streamer_array
+    }, function () {
+        console.log("saved streamer array");
+        restore_options();
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+            "streamer_array" : "null"
+        }, function (items) {
+            if(items.streamer_array == "null"){
+                console.log("null");
+                return;
+            }
+            streamer_array = items.streamer_array;
+            console.log(streamer_array[0]);
+            update_stream_list();
+    });
+}
+// document.addEventListener('DOMContentLoaded', restore_options);
+// document.getElementById('save').addEventListener('click', save_options);
 
 $( "#sortable" ).sortable();
 $( "#sortable" ).disableSelection();
 
-// Saves options to chrome.storage.sync.
-// function save_options() {
-// var color = document.getElementById('color').value;
-// var likesColor = document.getElementById('like').checked;
-// var firstname = document.getElementById('firstname').value;
-// var lastname = document.getElementById('lastname').value;
-// chrome.storage.sync.set({
-// favoriteColor : color,
-// likesColor : likesColor,
-// firstname : firstname,
-// lastname : lastname
-// }, function () {
-// // Update status to let user know options were saved.
-// var status = document.getElementById('status');
-// status.textContent = 'Options saved.';
-// setTimeout(function () {
-// status.textContent = '';
-// }, 750);
-// });
-// }
-
-// // Restores select box and checkbox state using the preferences
-// // stored in chrome.storage.
-// function restore_options() {
-// // Use default value color = 'red' and likesColor = true.
-// chrome.storage.sync.get({
-// favoriteColor : 'red',
-// likesColor : true,
-// firstname : '',
-// lastname : ''
-// }, function (items) {
-// document.getElementById('color').value = items.favoriteColor;
-// document.getElementById('like').checked = items.likesColor;
-// document.getElementById('firstname').value = items.firstname;
-// document.getElementById('lastname').value = items.lastname;
-// });
-// }
-// document.addEventListener('DOMContentLoaded', restore_options);
-// document.getElementById('save').addEventListener('click', save_options);
