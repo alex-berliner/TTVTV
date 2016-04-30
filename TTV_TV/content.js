3bf8904f55fd5ee61b3b0ecc0c9fe7c86046dd0f
Alex <alexberliner@gmail.com>
2016-04-30 13:16:16 -0400

    Stream switches are only attempted on pages that have twitch players now.

--- TTV_TV/content.js
+++ TTV_TV/content.js
@@ -14,26 +14,24 @@ document.addEventListener('DOMContentLoaded', function(){
  * Launches a stream heartbeat check request to background
  */
 function stream_hb_check(){
-    
     var stream_url = document.location.href;
     stream_url_split = stream_url.split(".tv/");
-    if(stream_url_split.length > 1 && stream_url_split[1].length != 0){
+    if($("#player").length > 0){
+        console.log("ping")
         var streamer_username = stream_url_split[1];
-        if(streamer_username.indexOf("/") <= -1) {
-            var page_data_init_obj = $($(".player")[0]).attr("data-initializing")
-            var twitch_data_initializing = 
-                page_data_init_obj == undefined || page_data_init_obj == "true"
-            
-            var page_stream_online_obj = $($(".player")[0]).attr("data-ended")
-            var twitch_stream_online = 
-                page_stream_online_obj != undefined &&
-                page_stream_online_obj == "false"
-            if (!twitch_data_initializing && !twitch_stream_online){
-                chrome.runtime.sendMessage({
-                    "message" : "change_url_req_msg"
-                });
-            }
-        }        
+        var page_data_init_obj = $($(".player")[0]).attr("data-initializing")
+        var twitch_data_initializing = 
+            page_data_init_obj == undefined || page_data_init_obj == "true"
+        
+        var page_stream_online_obj = $($(".player")[0]).attr("data-ended")
+        var twitch_stream_online = 
+            page_stream_online_obj != undefined &&
+            page_stream_online_obj == "false"
+        if (!twitch_data_initializing && !twitch_stream_online){
+            chrome.runtime.sendMessage({
+                "message" : "change_url_req_msg"
+            });
+        }
     }
 }
 
