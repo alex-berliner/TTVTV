e398e692750867cae2bb0cc38efbeffe23c5d659
Alex <alexberliner@gmail.com>
2016-05-01 14:38:56 -0400

    Removed db.js remnant crash.

--- TTV_TV/background.js
+++ TTV_TV/background.js
@@ -5,7 +5,6 @@ init();
  */
 function init(){
     chrome.runtime.onMessage.addListener(handle_comm_message);
-    init_db();
     //open settings page in new tab on browser button click
     chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
